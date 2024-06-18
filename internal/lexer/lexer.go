f4314e69d90f6a309ddae5ebe4f37f9a933ba340
hector3211e <hectororopesa5@gmail.com>
2024-06-17 20:30:58 -0400

    first commit

---
+++ internal/lexer/lexer.go
@@ -0,0 +1,121 @@
+package lexer
+
+import "go-interpreter/internal/token"
+
+type Lexer struct {
+	input        string
+	position     int // current position in input (points to current char)
+	readPosition int // current reading position in input (after current char)
+	ch           byte
+}
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.ReadChar()
+	return l
+}
+
+func (l *Lexer) ReadChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	l.skipWhiteSpaces()
+
+	switch l.ch {
+	case '=':
+		// if l.peekChar() == '=' {
+		//     tok = token.CreateToken(token.EQUAL)
+		// }
+		tok = token.CreateToken(token.ASSIGN, l.ch)
+	case '!':
+		tok = token.CreateToken(token.BANG, l.ch)
+	case ';':
+		tok = token.CreateToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = token.CreateToken(token.LPAREN, l.ch)
+	case ')':
+		tok = token.CreateToken(token.RPAREN, l.ch)
+	case ',':
+		tok = token.CreateToken(token.COMMA, l.ch)
+	case '+':
+		tok = token.CreateToken(token.PLUS, l.ch)
+	case '-':
+		tok = token.CreateToken(token.MINUS, l.ch)
+	case '*':
+		tok = token.CreateToken(token.ASTERIK, l.ch)
+	case '/':
+		tok = token.CreateToken(token.SLASH, l.ch)
+	case '{':
+		tok = token.CreateToken(token.LSQUIRLY, l.ch)
+	case '}':
+		tok = token.CreateToken(token.RSQUIRLY, l.ch)
+	case '<':
+		tok = token.CreateToken(token.LESSTHAN, l.ch)
+	case '>':
+		tok = token.CreateToken(token.GREATERTHAN, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifer()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = token.CreateToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.ReadChar()
+	return tok
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.ReadChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readIdentifer() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.ReadChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhiteSpaces() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.ReadChar()
+	}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
---
+++ internal/token/token.go
@@ -0,0 +1,74 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL TokenType = "ILLEGAL"
+	EOF     TokenType = "EOF"
+
+	// Identifiers + literals
+	IDENT TokenType = "IDNET" // add,foobar,x,y,..
+	INT   TokenType = "INT"   // 12345
+
+	// Operators
+	ASSIGN      TokenType = "="
+	EQUAL       TokenType = "=="
+	NOTEQUAL    TokenType = "!="
+	BANG        TokenType = "!"
+	PLUS        TokenType = "+"
+	MINUS       TokenType = "-"
+	SLASH       TokenType = "/"
+	ASTERIK     TokenType = "*"
+	LESSTHAN    TokenType = "<"
+	GREATERTHAN TokenType = ">"
+
+	// Delimiters
+	COMMA     TokenType = ","
+	SEMICOLON TokenType = ";"
+
+	LPAREN   TokenType = "("
+	RPAREN   TokenType = ")"
+	LSQUIRLY TokenType = "{"
+	RSQUIRLY TokenType = "}"
+
+	// Keywords
+	FUNCTION TokenType = "FUNCTION"
+	LET      TokenType = "LET"
+	IF       TokenType = "IF"
+	ELSE     TokenType = "ELSE"
+	TRUE     TokenType = "TRUE"
+	FLASE    TokenType = "FALSE"
+	RETURN   TokenType = "RETURN"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+	// FileName string
+	// Reader  io.Reader
+	Err error
+}
+
+func CreateToken(token TokenType, char byte) Token {
+	return Token{
+		Type:    token,
+		Literal: string(char),
+	}
+}
+
+var keyWords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"ture":   TRUE,
+	"false":  FLASE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keyWords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
